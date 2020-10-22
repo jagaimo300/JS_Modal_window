d717da4560272dc5dc0929d271a8480d13f894c3
Takahiro Ueda <tkhr6022@gmail.com>
2020-10-22 22:44:39 +0900

    Almost..

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 document.getElementById("openModal").addEventListener("click",() => {
+  document.getElementById("modal").classList.add("active");
+  document.getElementById("mask").classList.add("active");
 
+});
+document.getElementById("closeModal").addEventListener("click",() => {
+  document.getElementById("modal").classList.remove("active");
+  document.getElementById("mask").classList.remove("active");
 
 });
